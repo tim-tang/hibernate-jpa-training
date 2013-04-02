f8c75ea34fcc1b035f3988fb80ee5ac10b5937d2
tim.tang <tang.jilong@139.com>
2013-04-02 13:33:07 +0800

    Update criteria test

--- src/test/java/com/xplusz/criteria/CriteriaTests.java
+++ src/test/java/com/xplusz/criteria/CriteriaTests.java
@@ -3,12 +3,32 @@
  */
 package com.xplusz.criteria;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 /**
  * @author timtang
  *
  */
+@ContextConfiguration(locations = { "classpath:META-INF/spring/app-context.xml" })
+@RunWith(SpringJUnit4ClassRunner.class)
 public class CriteriaTests {
     
-    //TODO:
-
+    @PersistenceContext
+    private EntityManager entityManager;
+    
+    
+    @Test
+    @Rollback(false)
+    public void testCriteria(){
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        //TODO:
+    }
 }
